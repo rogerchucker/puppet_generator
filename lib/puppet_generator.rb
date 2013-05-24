465bd88cf5f717fe17c9ec96a6cbc522c2f6691f
Dennis Günnewig <dg1@vrnetze.de>
2013-05-24 08:06:28 +0200

    included actions

--- lib/puppet_generator.rb
+++ lib/puppet_generator.rb
@@ -7,5 +7,9 @@ require 'puppet_generator/output_file'
 require 'puppet_generator/package_definition_converter'
 require 'puppet_generator/puppet_package_definition'
 
+require 'puppet_generator/actions/convert_input_to_package_definition'
+require 'puppet_generator/actions/create_single_output_file'
+require 'puppet_generator/actions/read_input_file'
+
 module PuppetGenerator
 end
