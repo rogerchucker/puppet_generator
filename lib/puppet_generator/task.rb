829d5d8876f3da1873e36f7c62345e7571700c1c
Dennis Günnewig <dg1@vrnetze.de>
2013-05-27 11:10:54 +0200

    different types of tasks

--- lib/puppet_generator/task.rb
+++ lib/puppet_generator/task.rb
@@ -1,12 +1,28 @@
 module PuppetGenerator
   class Task
+    class << self
+      def define_type(type)
+        define_method "is_#{type}_task".to_sym do
+          @type = type.to_sym
+        end
+
+        define_method "is_#{type}_task?".to_sym do
+          @type == type.to_sym
+        end
+      end
+    end
 
     attr_reader :meta, :body
 
     def initialize
+      @type = :none
       @meta = {}
       @body = {}
     end
+
+    define_type :file
+    define_type :package
+
 
   end
 end
