2fd4993598f4802b165bebd9cb430ca1623069cd
Dennis Günnewig <dg1@vrnetze.de>
2013-05-27 13:44:02 +0200

    if statement was incorrect

--- lib/puppet_generator/actions/read_input_files.rb
+++ lib/puppet_generator/actions/read_input_files.rb
@@ -10,17 +10,16 @@ module PuppetGenerator
     def call(task)
       if is_directory? task.meta[:source]
         task.body[:input] = PuppetGenerator::InputDirectory.new( task.meta[:source] )
-        if is_stdin? task.meta[:source]
-          task.body[:input] = PuppetGenerator::InputStdIn.new
-        elsif is_file? task.meta[:source]
-          task.body[:input] = PuppetGenerator::InputFile.new( task.meta[:source] )
-        else
-          raise PuppetGenerator::Exceptions::InvalidSource
-        end
-
-        @app.call(task)
+      elsif is_stdin? task.meta[:source]
+        task.body[:input] = PuppetGenerator::InputStdIn.new
+      elsif is_file? task.meta[:source]
+        task.body[:input] = PuppetGenerator::InputFile.new( task.meta[:source] )
+      else
+        raise PuppetGenerator::Exceptions::InvalidSource
       end
 
+      @app.call(task)
     end
+
   end
 end
