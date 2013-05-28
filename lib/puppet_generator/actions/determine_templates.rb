95e4e2d0fa01d68b7633ad324f3b6963bb893ab4
Dennis Günnewig <dg1@vrnetze.de>
2013-05-28 07:06:20 +0200

    add hash of supported filters

--- lib/puppet_generator/actions/determine_templates.rb
+++ lib/puppet_generator/actions/determine_templates.rb
@@ -11,14 +11,18 @@ module PuppetGenerator
         task.body[:entry_creator] = Creators::FileEntry
         task.body[:templates][:class] = Templates::ClassFile
         task.body[:templates][:single] = Templates::SingleFile
+        task.body[:filter][:default] = Filter::Plain.new
       elsif task.is_package_task?
         task.body[:entry_creator] = Creators::PackageEntry
         task.body[:templates][:class] = Templates::ClassPackage
         task.body[:templates][:single] = Templates::SinglePackage
+        task.body[:filter][:default] = Filter::Plain.new
       elsif task.is_user_task?
         task.body[:entry_creator] = Creators::UserEntry
         task.body[:templates][:class] = Templates::ClassUser
         task.body[:templates][:single] = Templates::SingleUser
+        task.body[:filter][:etc_passwd] = Filter::EtcPasswd.new
+        task.body[:filter][:default] = Filter::Plain.new
       else
         raise PuppetGenerator::Exceptions::InternalError
       end
