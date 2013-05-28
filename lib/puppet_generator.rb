cc462fb08b92464893d25cc4a952c16b9c44b0ac
Dennis Günnewig <dg1@vrnetze.de>
2013-05-28 07:06:57 +0200

    added libs to main file

--- lib/puppet_generator.rb
+++ lib/puppet_generator.rb
@@ -29,6 +29,9 @@ require 'puppet_generator/templates/single_file'
 require 'puppet_generator/templates/class_user'
 require 'puppet_generator/templates/single_user'
 
+require 'puppet_generator/filter/etc_passwd'
+require 'puppet_generator/filter/plain'
+
 require 'puppet_generator/creators/user_entry'
 require 'puppet_generator/creators/package_entry'
 require 'puppet_generator/creators/file_entry'
