d52112f9f77ed90a122a6a6ab0c3665c286b0917
Dennis Günnewig <dg1@vrnetze.de>
2013-05-28 12:55:22 +0200

    arg. missed entries method again

--- lib/puppet_generator/actions/entry_to_package.rb
+++ lib/puppet_generator/actions/entry_to_package.rb
@@ -6,7 +6,7 @@ module PuppetGenerator
 
     def call(task)
       task.logger.debug(self.class.name){ "convert entries to puppet package objects" }
-      task.body = task.body.entries.collect { |e| PuppetPackage.new( e, task.meta[:module] , task.meta[:class] ) }
+      task.body = task.body.collect { |e| PuppetPackage.new( e, task.meta[:module] , task.meta[:class] ) }
 
       @app.call(task)
     end
