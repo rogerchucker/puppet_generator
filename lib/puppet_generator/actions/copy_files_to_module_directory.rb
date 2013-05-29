72ffb072701720b3540a252b8a27682b1b03e6ea
Max Meyer <dev@fedux.org>
2013-05-29 07:22:25 +0200

    renamed action

---
+++ lib/puppet_generator/actions/copy_files_to_module_directory.rb
@@ -0,0 +1,15 @@
+module PuppetGenerator
+  module Actions
+    class CopyFilesToModuleDirectory
+      def run(task)
+        binding.pry
+        src = task.body.collect { |e| e[:name] }
+        dst = File.join(task.meta[:module], 'files')
+
+        FileUtils.cp_r src , dst
+      rescue
+        raise Exceptions::ErrorDuringCopyFiles
+      end
+    end
+  end
+end
