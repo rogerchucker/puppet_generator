b72f9d1667c55d0d779d76e2fff232fa210278f7
Dennis Günnewig <dg1@vrnetze.de>
2013-06-03 10:34:18 +0200

    arg

--- lib/puppet_generator/setup/default_import_filter.r
+++
@@ -1,7 +0,0 @@
-module PuppetGenerator
-  module Setup
-    module DefaultImportFilter
-      def included(base)
-        Models::ImportFilter.create :null, Filter::Null.new
-        Models::ImportFilter.create :yaml, Filter::Yaml.new
-        end
