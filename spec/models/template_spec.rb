d3a0ccd735a002c396967cea0f0f44832df619f9
Dennis Günnewig <dg1@vrnetze.de>
2013-06-03 09:16:49 +0200

    migrate templates to new syntax

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -8,14 +8,19 @@ describe Models::Template do
   }
 
   it "returns a string of all active filters" do
-    Models::Template.create(:name1, 'Text1')
-    Models::Template.create(:name2, 'Text2')
+    Models::Template.create(:name1, '/path/text1')
+    Models::Template.create(:name2, '/path/text2')
 
     result = Models::Template.all_names_as_string(", ")
     expect(result).to eq("name1, name2")
 
     result = Models::Template.all_names_as_string
     expect(result).to eq("name1, name2")
+  end
+
+  it "loads all available templates on init" do
+    Models::Template.init
+    expect(Models::Template.find( name: :package, handles_one_element_only: true ).name).to eq(:package)
   end
 
 end
