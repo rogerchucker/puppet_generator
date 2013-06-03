df02f6dc72eaf546b8986648740e546c38e41a57
Dennis Günnewig <dg1@vrnetze.de>
2013-06-03 11:06:25 +0200

    check only relevant parts

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -5,17 +5,6 @@ describe Models::Action do
     Models::Action.clear
   }
 
-  it "returns a string of all active actions" do
-    Models::Action.create(:name1, 'Text1')
-    Models::Action.create(:name2, 'Text2')
-
-    result = Models::Action.all_names_as_string(", ")
-    expect(result).to eq("name1, name2")
-
-    result = Models::Action.all_names_as_string
-    expect(result).to eq("name1, name2")
-  end
-
   it "finds all available actions" do
     Models::Action.init
     action = Models::Action.find(:null)
--- spec/models/import_filter_spec.rb
+++ spec/models/import_filter_spec.rb
@@ -9,6 +9,8 @@ describe Models::ImportFilter do
   it "returns a string of all active filters" do
     Models::ImportFilter.create(:name1, 'Text1')
     Models::ImportFilter.create(:name2, 'Text2')
+    Models::ImportFilter.enable :name1
+    Models::ImportFilter.enable :name2
 
     result = Models::ImportFilter.all_names_as_string(", ")
     expect(result).to eq("name1, name2")
