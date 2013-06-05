76361290b38a1de6d2a771609a8f39f684b236ac
Dennis Günnewig <dg1@vrnetze.de>
2013-06-05 15:35:53 +0200

    fix export filter fs attr

--- lib/puppet_generator/export_filters/filesystem_attributes.rb
+++ lib/puppet_generator/export_filters/filesystem_attributes.rb
@@ -2,52 +2,55 @@ module PuppetGenerator
   module ExportFilters
     class FilesystemAttributes
       def convert(objects)
+
         objects.collect do |o|
+          path = o[:name]
 
-          if File.exists? o
-            fs_object_info = stats(o)
+          if File.exists? path
+            fs_object_info = stats(path)
 
-            o.merge( { 
-              type: type(object),
-              owner: owner,
-              mode: mode,
+            o.merge!( { 
+              type: type(path),
+              owner: owner(fs_object_info),
+              mode: mode(fs_object_info),
             } )
-          else
-            o
           end
+
+          o
         end
+
       end
 
       private
 
-      def stats(o)
-        File::Stat.new(o) 
+      def stats(path)
+        File::Stat.new(path) 
       end
 
-      def mode(o)
-        "%o" % o.mode
+      def mode(fs_object_info)
+        "%o" % fs_object_info.mode
       end
 
-      def owner(o)
-        Etc.getpwuid(o.uid).name
+      def owner(fs_object_info)
+        Etc.getpwuid(fs_object_info.uid).name
       end
 
-      def type(o)
-        if file?(o)
+      def type(path)
+        if file?(path)
           return 'file'
-        elsif directory?(o)
+        elsif directory?(path)
           return 'directory'
         else
           return 'file'
         end
       end
 
-      def file?(o)
-        FileTest.file? o
+      def file?(path)
+        FileTest.file? path
       end
 
-      def directory?(o)
-        FileTest.directory? o
+      def directory?(path)
+        FileTest.directory? path
       end
     end
   end
