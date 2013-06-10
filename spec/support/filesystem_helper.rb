2c608ef0b6cdbea4837f73bef28bd31e2f69250e
Dennis Günnewig <dg1@vrnetze.de>
2013-06-10 10:48:01 +0200

    moved rspec config

---
+++ spec/support/filesystem_helper.rb
@@ -0,0 +1,12 @@
+def examples_dir
+  File.expand_path( '../examples', __FILE__ )
+end
+
+require 'fedux_org/stdlib/filesystem'
+include FeduxOrg::Stdlib::Filesystem
+
+def root_directory
+  File.expand_path( '../../../', __FILE__ )
+end
+
+create_working_directory
