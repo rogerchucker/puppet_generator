6e23cf55c3f3a55fd2b9b23709304e7387757e92
Dennis Günnewig <dg1@vrnetze.de>
2013-06-10 09:25:50 +0200

    add check filter filesystem attributes

--- spec/middleware/apply_export_filters_spec.rb
+++ spec/middleware/apply_export_filters_spec.rb
@@ -7,20 +7,19 @@ describe PuppetGenerator::Middleware::ApplyExportFilters do
   }
   
   it "runs multiple filters" do
-    task = double('task')
-    task.stub(:body).and_return( [ { name: 'abcd' } ])
-    task.stub(:meta).and_return( { requested_export_filter: :filesystem_attributes } )
-    task.should_receive(:body=)
+    test_file = File.join( examples_dir, 'apply_export_filters', 'file.txt' )
+    user = ENV['USER']
+
+    task = double( 'task' )
+    task.stub( :body ).and_return( [ { name: test_file } ])
+    task.stub( :meta ).and_return( { requested_export_filter: :filesystem_attributes } )
+    task.should_receive( :body= ).with( [ {:name => test_file, :type => "file", :owner => user, :mode => "100644"} ] )
 
     app = double('app')
     app.stub(:call)
-    ###weiter
-
 
     mw = PuppetGenerator::Middleware::ApplyExportFilters.new(app)
     result = mw.call(task)
-
-    expect(result).to eq('')
 
   end
 
